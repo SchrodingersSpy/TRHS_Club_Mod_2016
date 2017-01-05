4a967f1bb5d8a000ef09213d96b86acc11da6e37
SchrodingersSpy <dap6789@gmail.com>
2017-01-04 18:48:35 -0700

    Please do what you're supposed to

--- src/main/java/epidemiccraft/lib/Constants.java
+++
@@ -1,9 +0,0 @@
-package epidemiccraft.lib;
-
-public class Constants {
-	public static final String MODID = "epidemiccraft";
-    public static final String MODNAME = "EpidemicCraft";
-    public static final String VERSION = "1.0.0";
-    public static final String CLIENT_PROXY_CLASS = "epidemiccraft.proxy.ClientProxy";
-    public static final String SERVER_PROXY_CLASS = "epidemiccraft.proxy.CommonProxy";
-}
